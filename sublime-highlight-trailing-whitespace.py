f14062b213cefa5fc83cb4778b2c3e117db4b798
Adrian L Lange <mail@p3lim.net>
2018-10-03 18:21:23 +0200

    Add color customization
    Fixes #4

--- sublime-highlight-trailing-whitespace.py
+++ sublime-highlight-trailing-whitespace.py
@@ -1,6 +1,13 @@
 import sublime
 import sublime_plugin
 
+import os
+import json
+
+NAMESPACE = 'HighlightTrailingWhitespace'
+DEFAULT_COLOR_SCHEME = 'Monokai.sublime-color-scheme'
+DEFAULT_COLOR = '#FF0000'
+
 def highlight_whitespace(view):
 	if view.size() > 1e5:
 		# avoid crashing on large files
@@ -21,10 +28,47 @@ def highlight_whitespace(view):
 						regions.remove(region)
 						break
 
-			view.add_regions('HighlightTrailingWhitespace', regions,
-				'invalid', '', sublime.DRAW_EMPTY | sublime.DRAW_NO_FILL | sublime.HIDE_ON_MINIMAP)
+			view.add_regions(NAMESPACE, regions, NAMESPACE, '',
+				sublime.DRAW_EMPTY | sublime.HIDE_ON_MINIMAP | sublime.DRAW_NO_FILL)
 		else:
-			view.erase_regions('HighlightTrailingWhitespace')
+			view.erase_regions(NAMESPACE)
+
+def update_colors(view):
+	# get the name of the current color scheme
+	path = view.settings().get('color_scheme') or DEFAULT_COLOR_SCHEME
+	if not path.startswith('Packages/'):
+		path = 'Packages/Color Scheme - Default/' + path
+
+	name = path.split('/')[-1].split('.')[0]
+
+	# create directory we'll save our own color schemes
+	scheme_path = os.path.join(sublime.packages_path(), 'User', NAMESPACE)
+	scheme_file = os.path.join(scheme_path, '{}.sublime-color-scheme'.format(name))
+	if not os.path.exists(scheme_path):
+		os.makedirs(scheme_path)
+
+	# create our override color scheme
+	style = {
+		'name': 'Highlight Trailing Whitespace',
+		'rules': [{
+			'name': 'Outline',
+			'scope': NAMESPACE,
+			'foreground': view.settings().get('highlight_trailing_whitespace_color', DEFAULT_COLOR),
+		}]
+	}
+
+	# attempt to create the color scheme, warning the user if it failed
+	try:
+		with open(scheme_file, 'wb', buffering=0) as file:
+			file.write(json.dumps(style, indent=4).encode('utf-8'))
+	except PermissionError as e:
+		sublime.ok_cancel_dialog('{} could not access file:\n{}'.format(NAMESPACE, e))
+		raise e
+	except OSError as e:
+		sublime.ok_cancel_dialog('{} encountered an OS error:\n{}'.format(NAMESPACE, e))
+		raise e
+
+	highlight_whitespace(view)
 
 class HighlightTrailingWhitespace(sublime_plugin.EventListener):
 	def on_modified_async(self, view):
@@ -42,4 +86,9 @@ class HighlightTrailingWhitespace(sublime_plugin.EventListener):
 def plugin_loaded():
 	for window in sublime.windows():
 		for view in window.views():
-			view.settings().add_on_change('HighlightTrailingWhitespace', lambda: highlight_whitespace(view))
+			view.settings().add_on_change(NAMESPACE, lambda: highlight_whitespace(view))
+			last_view = view
+
+	if last_view:
+		# no need to update colors for every view, it's global
+		update_colors(last_view)
